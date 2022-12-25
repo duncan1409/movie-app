e0667c3cb2e9c551f61a2b9500ffcc0f91cd4895
duncan1409 <duncansITworld@gmail.com>
2022-12-25 22:42:18 +0900

    axois API Loading

--- src/App.js
+++ src/App.js
@@ -1,6 +1,30 @@
+import React from "react";
+import axios from "axios";
 
-function App() {
-  return <div className="App" />
+class App extends React.Component {
+    state = {
+        isLoading: true,
+        movies: [],
+    };
+    getMovies = async () => {
+        //	영화 데이터 로딩!
+        const {
+            data: {
+                data: { movies },
+            },
+        } = await axios.get("https://yts.mx/api/v2/list_movies.json");
+        this.setState({movies: movies, isLoading: false});// movies: movies는 movies로 축약 가능
+
+    };
+    componentDidMount() {
+        this.getMovies();
+    }
+
+    render() {
+        const { isLoading } = this.state;
+
+        return <div>{isLoading ? "Loading..." : "We are Ready!"}</div>;
+    }
 }
 
 export default App;
